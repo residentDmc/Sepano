8b789fc6c7e7421e960f8aab8f2021c91e123496
andrioddeveloper <android.developer.70@gmail.com>
2023-11-07 17:36:14 +0330

    Implement  runtime permission BLUETOOTH_SCAN

--- app/src/main/java/com/example/sampleapplication/ui/view/fragment/SplashFragment.kt
+++ app/src/main/java/com/example/sampleapplication/ui/view/fragment/SplashFragment.kt
@@ -1,11 +1,18 @@
 package com.example.sampleapplication.ui.view.fragment
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -24,6 +31,7 @@ class SplashFragment : Fragment() {
     private lateinit var bottomSheetDialog: BottomSheetDialog
     private val handleErrorTools: HandleErrorTools by inject()
 
+    private val REQUEST_BLUETOOTH_SCAN_PERMISSION = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,6 +57,47 @@ class SplashFragment : Fragment() {
         initHandler()
     }
 
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_BLUETOOTH_SCAN_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                navController.popBackStack()
+                navController.navigate(R.id.homeFragment)
+            } else {
+                requireActivity().finishAffinity()
+            }
+        }
+    }
+
+    private fun checkBluetoothScanPermission(activity: Activity): Boolean {
+        val hasPermission = ContextCompat.checkSelfPermission(
+            activity,
+            Manifest.permission.BLUETOOTH_SCAN
+        )
+        return hasPermission == PackageManager.PERMISSION_GRANTED
+    }
+
+    // Function to request Bluetooth scan permission
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun requestBluetoothScanPermission(activity: Activity) {
+        val permission = Manifest.permission.BLUETOOTH_SCAN
+        if (ContextCompat.checkSelfPermission(
+                activity,
+                permission
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(permission),
+                REQUEST_BLUETOOTH_SCAN_PERMISSION
+            )
+        }
+    }
+
     @SuppressLint("SetTextI18n")
     private fun initSetView() {
         binding.txtVersionApp.text =
@@ -61,9 +110,22 @@ class SplashFragment : Fragment() {
 
     private fun initHandler() {
         Handler().postDelayed({
-          navController.popBackStack()
-          navController.navigate(R.id.homeFragment)
+            initResult()
         }, INTERVAL)
+    }
+
+    private fun initResult() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (checkBluetoothScanPermission(AppService.activity)) {
+                navController.popBackStack()
+                navController.navigate(R.id.homeFragment)
+            } else {
+                requestBluetoothScanPermission(AppService.activity)
+            }
+        }else{
+            navController.popBackStack()
+            navController.navigate(R.id.homeFragment)
+        }
     }
 
 }
